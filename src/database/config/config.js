fb3dec5cc36f90619584f506ffd518e3e8525f28
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-22 06:53:34 -0300

    chore: setup sequelize-cli

---
+++ src/database/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST } = process.env;
+
+module.exports = {
+  development: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: 'database_development',
+    host: MYSQL_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: 'database_test',
+    host: MYSQL_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: 'database_production',
+    host: MYSQL_HOST,
+    dialect: 'mysql',
+  },
+};
