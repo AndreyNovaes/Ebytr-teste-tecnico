4fab1f1202fdea76e67cb0bbb1d4763839a4b9d3
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-23 11:52:19 -0300

    test: add more syntax tests with more comments and a descritive information

--- src/tests/unit/databaseConfig.test.js
+++ src/tests/unit/databaseConfig.test.js
@@ -5,11 +5,19 @@ require('dotenv').config();
 
 const { sequelize: { config } } = indexModel;
 
-describe('tests if the database has the correct environment variables', () => {
+describe('tests if the database has received the environment variables', () => {
   const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_PORT } = process.env; 
   const { username, password, host, port } = config;
-  expect(MYSQL_HOST).to.equal(host);
-  expect(MYSQL_USER).to.equal(username);
-  expect(MYSQL_PASSWORD).to.equal(password);
-  expect(MYSQL_PORT).to.equal(port);
+  it('MYSQL_HOST .env envionment variable should be the same as config.host config', () => {
+    expect(MYSQL_HOST).to.equal(host);
+  });
+  it('MYSQL_USER .env envionment variable should be the same as config.username config', () => {
+    expect(MYSQL_USER).to.equal(username);
+  });
+  it('MYSQL_PASSWORD .env envionment variable should be the same as config.password config', () => {
+    expect(MYSQL_PASSWORD).to.equal(password);
+  });
+  it('MYSQL_PORT .env envionment variable should be the same as config.port config', () => {
+    expect(MYSQL_PORT).to.equal(port);
+  });
 });
