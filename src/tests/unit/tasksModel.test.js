ec0b56b6bfbb043380346104ad1b1919afef4d91
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-23 08:45:35 -0300

    test: add new unit test for model tasks

---
+++ src/tests/unit/tasksModel.test.js
@@ -0,0 +1,16 @@
+const { describe } = require('mocha');
+const {
+  sequelize, dataTypes, checkModelName, checkPropertyExists,
+} = require('sequelize-test-helpers');
+const tasksModel = require('../../database/models/tasks');
+
+describe('model tasks', () => {
+  const Tasks = tasksModel(sequelize, dataTypes);
+  const tasks = new Tasks();
+
+  checkModelName(tasks, 'tasks');
+
+  context('tasks properties', () => {
+    ['name', 'description', 'status'].forEach(checkPropertyExists(tasks));
+  });
+});
