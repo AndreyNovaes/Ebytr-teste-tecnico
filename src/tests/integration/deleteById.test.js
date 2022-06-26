f26c423f2fa4f8c20b6d243a16529381d4f351ec
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-26 13:02:03 -0300

    test: add test deleteById sucessfully case, using only the test database

---
+++ src/tests/integration/deleteById.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const App = require('../../app');
+const { before, after } = require('mocha');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('HTTP DELETE route /tasks/:id', () => {
+  const fakeBody = { name: 'test', description: 'test' };
+  let idWhatExists;
+  before(async () => {
+    const response = await chai.request(App).post('/tasks').send(fakeBody);
+    idWhatExists = response.body.id;
+  });
+  it('should return the message of Task :id deleted', async () => {
+    const response = await chai.request(App).delete(`/tasks/${idWhatExists}`);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('message');
+    expect(response.body.message).to.equal(`Task ${idWhatExists} deleted`);
+  });
+});
