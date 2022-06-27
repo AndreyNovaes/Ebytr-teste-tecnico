de535378819b51e9dbede90458a6089270a8bd3e
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-06-27 12:21:02 -0300

    doc: add image in repo to use in readme doc

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const tasksRouter = require('./handleTasks/Tasks.router');
 const App = Express();
 
 App.use(Express.json());
+App.use('/', (req, res) => { res.json('Hello World!'); });
 App.use('/tasks', tasksRouter);
 
 module.exports = App;
