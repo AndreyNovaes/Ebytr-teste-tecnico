3481380afc69f91d5b367f83829d4eeec028415d
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-07-01 09:29:59 -0300

    fix: fix port undefined possibility

--- src/server.js
+++ src/server.js
@@ -2,5 +2,6 @@ const App = require('./app');
 require('dotenv').config();
 
 const { PORT } = process.env;
+const port = PORT || 3001;
 
-App.listen(PORT, () => { console.log(`Server listening on database_development port: ${PORT}`); });
+App.listen(port, () => { console.log(`Server listening on database_development port: ${port}`); });
