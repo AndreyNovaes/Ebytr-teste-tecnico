e456f5b353e6319a28f480e6892856922c5ae5b3
Andrey De Novaes Ferreira <87386380+GTmudin@users.noreply.github.com>
2022-07-04 10:49:14 -0300

    feat: add requests in services front-end requests

---
+++ front-end/src/services/requests.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: `http://localhost:${process.env.PORT || 3001}`,
+});
+
+export const getAll = async () => {
+  const response = await api.get('/');
+  return response.data;
+};
+
+export const getById = async (id) => {
+  const response = await api.get(`/${id}`);
+  return response.data;
+};
+
+export const createOne = async (data) => {
+  const response = await api.post('/', data);
+  return response.data;
+};
+
+export const updateById = async (id, data) => {
+  const response = await api.put(`/${id}`, data);
+  return response.data;
+};
+
+export const deleteById = async (id) => {
+  const response = await api.delete(`/${id}`);
+  return response.data;
+};
+
+export const updateStatusById = async (id, data, status) => {
+  const response = await api.put(`/${id}/${status}`);
+  return response.data;
+};
+
+export const getAllByStatus = async (status) => {
+  const response = await api.get(`/?filter=${status}`);
+  return response.data;
+};
+
+export const getAllByName = async (name) => {
+  const response = await api.get(`/?filter=${name}`);
+  return response.data;
+};
